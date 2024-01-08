0f59bfad5eaff43759c4b6b6ef2db6794e014401
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-08 20:59:11 -0300

    :sparkles: verify that the user sended a non-empty string, finishing user interaction

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,6 +4,8 @@
 #define BAUD_RATE 115200
 #define START_DELAY_MS 500
 
+extern String read_serial_user_input_string(void);
+
 void setup(void) {
   Serial.begin(BAUD_RATE);
 
@@ -12,5 +14,11 @@ void setup(void) {
 
 void loop(void) {
   String user_input = read_serial_user_input_string();
+
+  if (user_input.length() == 0) {
+    println("err : command not specified");
+    return;
+  }
+
   println(user_input);
 }
