93a1325f1c6bfd3243836d1aec575d9387adafa0
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-08 20:51:08 -0300

    :recycle: added a little more helper definitions and macros to the main sketch file

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,12 +1,16 @@
+#define print(c) Serial.print(c)
+#define println(c) Serial.println(c)
+
+#define BAUD_RATE 115200
 #define START_DELAY_MS 500
 
 void setup(void) {
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
 
   delay(START_DELAY_MS);
 }
 
 void loop(void) {
   String user_input = read_serial_user_input_string();
-  Serial.println(user_input);
+  println(user_input);
 }
