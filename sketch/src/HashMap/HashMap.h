353dd8ed9e864a63b43addf5635ebe6c690d4075
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-09 11:38:19 -0300

    :bug: the arduino compiler doesn't support template types outside the header file

--- sketch/src/HashMap/HashMap.h
+++ sketch/src/HashMap/HashMap.h
@@ -6,13 +6,23 @@
 template <typename K, typename V>
 class HashMap {
 private:
-  LinkedList<K> _keys_list;
-  LinkedList<V> _vals_list;
+  LinkedList<K>* _keys_list;
+  LinkedList<V>* _vals_list;
 
 public:
-  inline HashMap(void) : _keys_list(nullptr), _vals_list(nullptr) {}
+  inline HashMap(void) : _keys_list(new LinkedList<K>()), _vals_list(new LinkedList<V>()) {}
   inline ~HashMap(void) {}
 
-  void add(K, V);
-  status_t<V> get(K);
+  void add(K key, V val) {
+    _keys_list->add(key);
+    _vals_list->add(val);
+  }
+
+  status_t<V> get(K key) {
+    for (u8 i = 0; i < _keys_list->size(); ++i)
+      if (_keys_list->get(i) == key)
+        return {true, _vals_list->get(i)};
+
+    return {false};
+  }
 };
