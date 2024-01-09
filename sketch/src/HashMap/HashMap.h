0bee6b013748228767e95ba1ad283b2956ed8765
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-08 22:07:00 -0300

    :sparkles: new object that simulates a hashmap, which will be the shell controller

---
+++ sketch/src/HashMap/HashMap.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Arduino.h>
+#include <LinkedList.h>
+#include "../types/types.h"
+
+template <typename K, typename V>
+class HashMap {
+private:
+  LinkedList<K> _keys_list;
+  LinkedList<V> _vals_list;
+
+public:
+  inline HashMap(void) : _keys_list(nullptr), _vals_list(nullptr) {}
+  inline ~HashMap(void) {}
+
+  void add(K, V);
+  status_t<V> get(K);
+}
