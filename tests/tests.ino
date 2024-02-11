a3356f177770db45f2a0b18a8bd34c9aa17cfec8
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-02-11 20:38:34 -0300

    test: Printing something on the terminal for debugging propurses

--- tests/tests.ino
+++ tests/tests.ino
@@ -1,6 +1,10 @@
 //no tests added yet...
 
 void setup(void) {
+    Serial.begin(115200);
+    Serial.println("Hello world!");
+    Serial.println("\nThe tests aren't implement yet");
+    Serial.println("but they will soon! :D");
 }
 
 void loop(void) {
