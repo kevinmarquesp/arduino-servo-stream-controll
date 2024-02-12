fcf7ebc7ec0d84799a8630f3a9bb36248ad4b198
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-02-12 19:41:49 -0300

    test-tube (begin): When the clean up and overwrite behavior of that command

--- tests/_begin.ino
+++ tests/_begin.ino
@@ -1,73 +1,53 @@
 #include <LinkedList.h>
 #include <ParallelServo.h>
 
-test(test_suit, assert_true) {
-  assertTrue(true);
-}
+#include "src/commands/commands.h"
 
-// TODO: figure out how this test units will include the local `src/` loca sketch library
-
-/*******************************************************************************
-test(commands_begin_fills_servos_list) {
+test(BeginCommand, commands_begin_fills_servos_list) {
   // Arrange
   LinkedList<ParallelServo*> servos_list;
-  String args = "3 0 180"; // Sample arguments for one servo
+  String args = "5:0:180 6:0:180 7:0:180"; // Sample arguments for one servo
   u8 argc = 3;
 
   // Act
   commands::begin(servos_list, argc, args);
 
   // Assert
-  assertTrue(servos_list.size() > 0); // Check if the servos_list is not empty after calling the function
+  assertEqual(servos_list.size(), 3); // Check if the servos_list is not empty after calling the function
 }
 
-test(commands_begin_clears_list_when_already_filled) {
+test(BeginCommand, commands_begin_clears_list_when_already_filled) {
   // Arrange
   LinkedList<ParallelServo*> servos_list;
-  servos_list.add(new ParallelServo()); // Adding a dummy servo to the list
+  commands::begin(servos_list, 1, "5:0:180"); // Adding a dummy servo to the list
   commands::begin(servos_list, 0, ""); // Call the function to clear the list
   
   // Assert
   assertEqual(servos_list.size(), 0); // Check if the list is empty after calling the function
 }
 
-test(commands_begin_adds_new_servos_to_empty_list) {
+test(BeginCommand, commands_begin_ignores_ignored_arguments) {
   // Arrange
   LinkedList<ParallelServo*> servos_list;
-  String args = "3, 0, 180"; // Sample arguments for one servo
-  u8 argc = 1;
+  String args = "5:0:180 @6:0:180 7:0:180"; // Ignored argument
+  u8 argc = 3;
 
   // Act
   commands::begin(servos_list, argc, args);
 
   // Assert
-  assert_equal(servos_list.size(), 1); // Check if the list size is correct after adding a servo
+  assertEqual(servos_list.size(), 2); // Check if the list size remains unchanged
 }
 
-test(commands_begin_adds_new_servos_to_non_empty_list) {
+test(BeginCommand, commands_begin_overwrites_filled_list_with_new_servos) {
   // Arrange
   LinkedList<ParallelServo*> servos_list;
-  servos_list.add(new ParallelServo()); // Adding a dummy servo to the list
-  String args = "3, 0, 180"; // Sample arguments for one servo
-  u8 argc = 1;
+  commands::begin(servos_list, 2, "5:0:180 6:0:180"); // Adding a dummy servo to the list
 
   // Act
-  commands::begin(servos_list, argc, args);
+  commands::begin(servos_list, 1, "5:0:180"); // Sample arguments for one servo
 
   // Assert
-  assert_equal(servos_list.size(), 1); // Check if the list size is correct after adding a servo
+  assertEqual(servos_list.size(), 1); // Check if the list size is correct after adding a servo
+  // Additional assertions can be made to ensure the correct servo data is stored in the list
 }
-
-test(commands_begin_ignores_ignored_arguments) {
-  // Arrange
-  LinkedList<ParallelServo*> servos_list;
-  String args = "-1, 0, 180"; // Ignored argument
-  u8 argc = 1;
-
-  // Act
-  commands::begin(servos_list, argc, args);
-
-  // Assert
-  assert_equal(servos_list.size(), 0); // Check if the list size remains unchanged
-}
-*******************************************************************************/
