99c37ed031bb18f10e83acf38c6a4009b2ee8158
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-02-12 12:42:30 -0300

    ref: Display an 'EMPTY' string if the command has 0 arity

--- sketch/_serial_display.ino
+++ sketch/_serial_display.ino
@@ -8,7 +8,7 @@ inline void display_command_arity(String command, u8 argc, String argv) {
   print("/");
   print(argc);
   print(" -> ");
-  println(argv);
+  println(argv.length() <= 0 ? "NULL" : argv);
 }
 
 inline void display_command_dosent_exist_error(String command, u8 argc) {
